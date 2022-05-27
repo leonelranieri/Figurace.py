d93d04826744d2f5440a4a2c2892c9cf8ba0ae81
Lucas Guerendiain <lucasguere@hotmail.com>
2022-05-27 20:35:52 -0300

    cambios en la configuracion para que funcione como deberia, y un par en el menu de inicio para que no explote

--- src/core/configuracion.py
+++ src/core/configuracion.py
@@ -8,31 +8,30 @@ from json import (load as jsonload, dump as jsondump)
     en proceso de desarrollo
 """
 
-ARCHIVO_CONFIG = os.path.join(os.getcwd(), "src\core", "data", "config.json")
-#la idea es que "dificultad" sirva para cambiar varios aspectos de la configuracion a la vez, pero que puedas seguir modificando cada una por separado 
-DEFAULT_CONFIG = {
-                "tiempo" : 15, 
-                "rondas" : 10, 
-                "suma" : 2, 
-                "resta" : 2, 
-                "cara" : 4, 
-                "dificultad" : "normal"
-}
-DEFAULT_EASY = {
-                "tiempo" : 20, 
-                "rondas" : 5, 
-                "suma" : 3, 
-                "resta" : 1, 
-                "cara" : 5, 
-                "dificultad" : "facil"
-}
-DEFAULT_DIFFICULT = {
-                "tiempo" : 10, 
-                "rondas" : 45, 
-                "suma" : 1, 
-                "resta" : 3, 
-                "cara" : 3, 
-                "dificultad" : "dificil"
+ARCHIVO_CONFIG = os.path.join(os.getcwd(), "src", "core", "data", "config.json")
+
+DEFAULT_CONFIG = { 
+                    "facil" : {
+                                "tiempo" : 20, 
+                                "rondas" : 5, 
+                                "suma" : 3, 
+                                "resta" : 1, 
+                                "cara" : 5, 
+                    },
+                    "normal" : {
+                                "tiempo" : 15, 
+                                "rondas" : 10, 
+                                "suma" : 2, 
+                                "resta" : 2, 
+                                "cara" : 4, 
+                    },
+                    "dificil" : {
+                                "tiempo" : 10, 
+                                "rondas" : 45, 
+                                "suma" : 1, 
+                                "resta" : 3, 
+                                "cara" : 3, 
+                    }
 }
 KEY_CONFIG = {
             "tiempo" : "-TIEMPO-",
@@ -40,9 +39,8 @@ KEY_CONFIG = {
             "suma" : "-SUMA-", 
             "resta" : "-RESTA-", 
             "cara" : "-CARACT-", 
-            "dificultad" : "-DIFI-"
 }
-
+CHOICES = ("facil", "normal", "dificil")
 
 def carga_config():
     try:
@@ -56,7 +54,7 @@ def carga_config():
 def guarda_config(configuracion, values):
     if values:
         for key in KEY_CONFIG:
-            configuracion[key] = values[KEY_CONFIG[key]]
+            configuracion[values["-DIFI-"][0]][key] = values[KEY_CONFIG[key]]
     
     with open(ARCHIVO_CONFIG, "w") as salida:
         jsondump(configuracion, salida)
@@ -64,8 +62,11 @@ def guarda_config(configuracion, values):
 def crear_ventana(configuracion):
     layout = [
         [sg.Text("Configuracion", font="Any 25", pad=((10, 0), 30))],
-        [sg.Text("tiempo limite por ronda", pad=((10, 5), 20), 
-                    text_color="black"), 
+        [sg.Text("seleccionar dificultad"),
+                sg.Listbox(CHOICES, size=(15, len(CHOICES)),
+                key=("-DIFI-"), enable_events=True)
+        ],
+        [sg.Text("tiempo limite por ronda", pad=((10, 5), 20)), 
                     sg.OptionMenu(values=("10", "15", "20"), 
                     size=(5, 1), key="-TIEMPO-")
         ],
@@ -88,11 +89,6 @@ def crear_ventana(configuracion):
                     sg.OptionMenu(values=("3", "4", "5"), 
                     size=(3, 1), key=("-CARACT-"))
         ],
-        [sg.Text("dificultad general", pad=((10, 5), 20)), 
-                    sg.OptionMenu(values=("facil", "normal", 
-                    "dificil", "Custom"),
-                    size=(10, 1), key=("-DIFI-"))
-        ],
         [sg.Text(size=(5, 1))],
         [sg.Button("Save"), sg.Button("Cancel")]
     ]
@@ -101,12 +97,8 @@ def crear_ventana(configuracion):
     window = sg.Window("Configuracion", layout, 
             font="Any 18", margins=(100, 50), finalize="TRUE")
 
-    for key in KEY_CONFIG:
-        window[KEY_CONFIG[key]].update(value=configuracion[key])
-
     return window
 
-###falta implementar el tab de dificultad, pero no se me ocurre como
 def main():
     configuracion = carga_config()
     window = crear_ventana(configuracion)
@@ -118,5 +110,9 @@ def main():
         if event == "Save":
             guarda_config(configuracion, values)
             window.close()
+        if values["-DIFI-"]:
+            for key in KEY_CONFIG:
+                window[KEY_CONFIG[key]].update(configuracion[values["-DIFI-"][0]][key])
+            window.refresh()
 
     window.close()
